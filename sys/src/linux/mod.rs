7d138cab682c58e7366fdb9ea373e3a43acf261e
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2024-06-04 12:10:11 +0200

    Add bindings for s390x

--- sys/src/linux/mod.rs
+++ sys/src/linux/mod.rs
@@ -2,5 +2,6 @@
 #[cfg_attr(target_arch = "x86_64", path = "x86_64.rs")]
 #[cfg_attr(target_arch = "arm", path = "arm.rs")]
 #[cfg_attr(target_arch = "aarch64", path = "aarch64.rs")]
+#[cfg_attr(target_arch = "s390x", path = "s390x.rs")]
 mod unsupported;
 pub use unsupported::*;
